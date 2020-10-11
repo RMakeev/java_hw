85fe77c036092d1202da45aaf1d6c479131aa6ce
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-10-12 02:09:51 +0300

    Create GroupModificationTest

---
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.hw.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.hw.adressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+  @Test
+  public void testGroupModification() {
+    app.getNavifationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+
+  }
+}
