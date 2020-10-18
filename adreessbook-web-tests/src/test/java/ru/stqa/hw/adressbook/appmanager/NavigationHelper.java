889dfa81de2a2f64a0c77137b3e8ac33fe56b542
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-10-18 20:05:29 +0300

    оптимизированы переходы между страницами

--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/appmanager/NavigationHelper.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/appmanager/NavigationHelper.java
@@ -11,7 +11,13 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
+
   }
 
   public void gotoContactPage() {
@@ -19,6 +25,9 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
