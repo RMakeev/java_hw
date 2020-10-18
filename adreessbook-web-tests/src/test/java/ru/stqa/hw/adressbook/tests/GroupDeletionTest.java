2634a8844eb4685855b26c9110c9175c104c3643
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-10-18 20:26:14 +0300

    Реализована проверка и обеспечение предусловий

--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/GroupDeletionTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/GroupDeletionTest.java
@@ -2,12 +2,16 @@ package ru.stqa.hw.adressbook.tests;
 
 
 import org.testng.annotations.Test;
+import ru.stqa.hw.adressbook.model.GroupData;
 
 public class GroupDeletionTest extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavifationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
