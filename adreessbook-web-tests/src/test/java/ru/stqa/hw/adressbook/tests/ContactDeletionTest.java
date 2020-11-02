fe705ca2e3ef5c9bab9f4449c84ace4d8f576510
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-11-02 22:38:44 +0300

    КОНТАКТЫ 4.6   Сравнение коллекций

--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactDeletionTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactDeletionTest.java
@@ -24,6 +24,9 @@ public class ContactDeletionTest extends TestBase {
     app.getNavifationHelper().goToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() -1);
+    Assert.assertEquals(before, after);
   }
 
 
