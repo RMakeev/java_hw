a30ba254b48c7a2ddae82122d0cc750991def480
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-11-10 01:26:29 +0300

    Лекция 5.5.
    группы

--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/model/GroupData.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/model/GroupData.java
@@ -15,6 +15,20 @@ public class GroupData {
   }
 
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
   public GroupData withId(int id) {
     this.id = id;
     return this;
@@ -47,19 +61,6 @@ public class GroupData {
     return footer;
   }
 
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
-  }
 
   @Override
   public String toString() {
