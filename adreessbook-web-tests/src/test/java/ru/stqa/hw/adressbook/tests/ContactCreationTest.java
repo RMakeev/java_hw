0132fd528809951d53e7d7a4d0c6b73e525ad1ac
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-11-10 21:26:49 +0300

    Лекция 5.8 для контактов

--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactCreationTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactCreationTest.java
@@ -17,13 +17,23 @@ public class ContactCreationTest extends TestBase {
     Contacts before = app.contact().all();
     ContactData contact = new ContactData().withFirstname("Roman").withLastname("Makeev").withMobile("88008008888").withEmail("test@test.ts.test");
     app.contact().create(contact);
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
 
   }
 
+  @Test
+  public void testBadContactCreation() throws Exception {
+    Contacts before = app.contact().all();
+    ContactData contact = new ContactData().withFirstname("Roman'").withLastname("Makeev").withMobile("88008008888").withEmail("test@test.ts.test");
+    app.contact().create(contact);
+    assertThat(app.contact().count(), equalTo(before.size()));
+    Contacts after = app.contact().all();
+    assertThat(after, equalTo(before));
+
+
+  }
 }
--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactModificationTests.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactModificationTests.java
@@ -31,8 +31,8 @@ public class ContactModificationTests extends TestBase {
     ContactData contact = new ContactData()
             .withId(modifiedContact.getId()).withFirstname("Test").withLastname("Testov").withMobile("88008008888").withEmail("test@test.ts");
     app.contact().modify(contact);
+    assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.contact().all();
-    assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
   }
 }
