d5020aa169e5a4a88968826804ab191c20fb3414
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-11-15 01:30:52 +0300

    6.5 - контакты

--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactCreationTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/ContactCreationTest.java
@@ -7,7 +7,10 @@ import ru.stqa.hw.adressbook.model.ContactData;
 import ru.stqa.hw.adressbook.model.Contacts;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
@@ -19,11 +22,16 @@ public class ContactCreationTest extends TestBase {
 
   @Test
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData().withFirstname("Roma").withLastname("Testov").withMobilePhone("89-89-78").withEmail("testa@ts.ts")});
-    list.add(new Object[] {new ContactData().withFirstname("Rita").withLastname("Testova").withMobilePhone("555555").withEmail("tes1t@test.ts")});
-    list.add(new Object[] {new ContactData().withFirstname("Rima").withLastname("Kek").withMobilePhone("02").withEmail("Rima@Rima.Rima")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null ) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withHomePhone(split[2])
+              .withMobilePhone(split[3]).withEmail(split[4]).withAddress(split[5])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
