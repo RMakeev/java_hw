afa5a77fb5382f3a7ca6df804eb19127c41e4241
RMakeev <49813712+RMakeev@users.noreply.github.com>
2020-11-14 23:47:52 +0300

    Лекция 6.4.  - группы

--- adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/GroupCreationTest.java
+++ adreessbook-web-tests/src/test/java/ru/stqa/hw/adressbook/tests/GroupCreationTest.java
@@ -5,17 +5,28 @@ import ru.stqa.hw.adressbook.model.GroupData;
 import ru.stqa.hw.adressbook.model.Groups;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreationTest extends TestBase {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new  GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new  GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new  GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test1");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
